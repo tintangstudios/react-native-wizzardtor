15213770e30fed1c16888f4f18b6dc704b1799c8
adderly <adderlygonzalez@gmail.com>
2024-09-22 14:42:20 -0400

    feat:  Export types & default commit changes

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,11 @@
 // Your main export will be here
-import React from "react";
-import { View, Text } from "react-native";
 
-import { useWizardState } from "./WizzardState";
+import {
+  useWizardState,
+  RouterStepProp,
+  createCustomWizzardState,
+  RouterProps,
+} from "./WizzardState";
 
-export default function VarComp() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  );
-}
-
-export { useWizardState, VarComp };
+export { useWizardState, createCustomWizzardState };
+export type { RouterStepProp, RouterProps };
